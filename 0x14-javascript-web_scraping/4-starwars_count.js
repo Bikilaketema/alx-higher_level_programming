609a87b7b00a819e36432567e208df660e529833
bikilaketema <bikilaketema94@gmail.com>
2023-04-25 14:51:33 +0300

    Debug task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
     return;
   }
-
-  const movies = JSON.parse(body).results;
+  
+  const films = JSON.parse(body).results;
   const characterId = '18';
   let count = 0;
-
-  for (const movie of movies) {
-    const characters = movie.characters;
-
+  
+  for (const film of films) {
+    const characters = film.characters;
     if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
       count++;
     }
   }
-
+  
   console.log(count);
 });
